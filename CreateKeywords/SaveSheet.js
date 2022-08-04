bf1cc94b3a4e6f6e4e35e193e58fe9af3ba23f0f
WePack <wepack@Mac-mini.localdomain>
2022-08-04 10:50:04 +0200

    save sheet to drive

---
+++ CreateKeywords/SaveSheet.js
@@ -0,0 +1,32 @@
+// Save active sheet
+function saveSheetToDriveFolder(){  
+  // Ask user
+  let ui = SpreadsheetApp.getUi();
+  const userInput = ui.prompt("Please, save file as Type Model Country (EG. Smartphone - iPhone Y - SE) ");
+  let newFileName = userInput.getResponseText();
+  // The active spreadsheet
+  const currentSheet = SpreadsheetApp.getActiveSheet();  
+  const sheetName = currentSheet.getName();
+  const spreadsheetID = currentSheet.getParent().getId();
+
+  // Get sheetname so we can find the correct folder
+  if(sheetName == 'Smartphone Template'){
+    var googleDriveFolderId = '1DBwFu0MblZjCI5zP1deDadfABuIArYPr'
+  }
+  if(sheetName == 'Smartwatch Template'){
+    var googleDriveFolderId = '1_WB_JdQ5dlKc2UJ0oJd5OF9SNsEWweKK'
+  }
+  const googleDriveFolder = DriveApp.getFolderById(googleDriveFolderId);
+  // copy the spreadsheet
+  // Append date to the newFileName sheet name, so we don't accidentally overwrite others
+  newFileName = newFileName + ' - ' + Math.floor(Date.now() / 1000);
+
+  const spreadSheetCopy = SpreadsheetApp.open(DriveApp.getFileById(spreadsheetID).makeCopy(newFileName, googleDriveFolder));
+  // Delete the sheet we don't need in the copy (We only want the saved text)
+  const sheetsCopy = spreadSheetCopy.getSheets();
+  for(i = 0; i < sheetsCopy.length; i++){
+    if(sheetsCopy[i].getSheetName() != sheetName){
+      spreadSheetCopy.deleteSheet(sheetsCopy[i]);
+    }
+  }
+}
